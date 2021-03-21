541460509b9463095f0f62a1be1aa54183215e76
Daniel Kosykh <dankosykh@gmail.com>
2021-03-21 15:01:59 -0700

    set up routes to products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-const productURL =
+const PRODUCT_URL = 'http://localhost:3001/products/';
+
+router.get('/', (err, response) => {
+  console.log(err)
+  console.log(response)
+  console.log('hello from client')
+})
+
+
+// axios.get('http://localhost:3001/products/')
+// .then((response) => {
+//   console.log(response.data);
+//   res.send('w/e');
+// })
+// .catch((error) => {
+//   console.log(error);
+//   res.status(500).send(error);
+// })
