a0e0c33adb59a760503e3e18acda3a3db5a91058
Daniel Kosykh <dankosykh@gmail.com>
2021-03-30 08:38:56 -0700

    add nginx config file

--- server/routes/styleRoutes.js
+++ server/routes/styleRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const axios = require('axios');
+const aws = require('./config.js');
 
 router.get('/*', (req, res) => {
-  axios.get(`http://127.0.0.1:3005/styles`, { params: req.params[0] })
+  axios.get(`${aws.PRODUCTS}:${aws.PRODUCTPORT}/styles`, { params: req.params[0] })
     .then(response => res.send(response.data[0]))
     .catch(err => console.log(err))
 });
